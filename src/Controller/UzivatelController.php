17abb4e29a1aef34115e392a07dc200b3842d8f9
honzaseda <honza.seda@email.cz>
2016-03-16 20:42:52 +0100

    Vytvoření controlleru Uzivatel pro veškeré uživatelské akce (přihlašování, registrace, ...)

---
+++ src/Controller/UzivatelController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Seda
+ * Date: 16. 3. 2016
+ * Time: 20:33
+ */
+
+namespace App\Controller;
+
+
+class UzivatelController extends AppController
+{
+    function login()
+    {
+
+    }
+}
