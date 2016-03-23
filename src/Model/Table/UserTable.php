07e5455dc1dc5b7d1fab668f5e1c408cce7ff40c
Lukado <lukado222@seznam.cz>
2016-03-23 21:32:41 +0100

    Smazání nepatřičných funkcí

--- src/Model/Table/UserTable.php
+++ src/Model/Table/UserTable.php
@@ -8,26 +8,12 @@
 namespace App\Model\Table;
 
 use Cake\ORM\Table;
-use Cake\Validation\Validator;
 
 class UserTable extends Table
 {
-
     public function initialize(array $config)
     {
         $this->table('user');
         //$this->addBehavior('Timestamp');
-    }
-
-    public function validationDefault(Validator $validator)
-    {
-        return $validator
-            ->notEmpty('email', 'A username is required')
-            ->notEmpty('password', 'A password is required')
-            ->notEmpty('role', 'A role is required')
-            ->add('role', 'inList', [
-                'rule' => ['inList', ['admin', 'author']],
-                'message' => 'Please enter a valid role'
-            ]);
     }
 }
