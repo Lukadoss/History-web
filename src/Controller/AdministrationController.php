059f6d943444f071cd06e945ced4d0240210aaa3
Lukado <lukado222@seznam.cz>
2016-04-17 22:15:26 +0200

    Administrace done

--- src/Controller/AdministrationController.php
+++ src/Controller/AdministrationController.php
@@ -28,25 +28,56 @@ class AdministrationController extends AppController
         $sources = $this->Sources->find('all')
             ->contain([
                 'Users' => function ($q) {
-                    return $q
-                        ->select(['forename', 'surname', 'email']);
+                    return $q->select(['forename', 'surname', 'email']);
                 }
             ])
-            ->where(['onHold' => true]);
+            ->andWhere(['onHold' => true]);
         $this->set('sources', $sources);
     }
 
-    function accept(){
+    function accept($sourceID = null){
+        if ($sourceID != null){
+            $source = $this->Sources->get($sourceID);
+
+            if($source->onHold) $source->onHold = 0;
+            if($this->Sources->save($source)) {
+                $this->Flash->success('Příspěvek "' . $source->name . '" byl úspěšně schválen!');
+            }else{
+                $this->Flash->error('Chyba při potvrzování :(');
+            }
+        }
         return $this->redirect($this->referer());
     }
 
-    function edit(){
-        return $this->redirect($this->referer());
+    function edit($sourceID = null){
+        if ($sourceID != null) {
+            $source = $this->Sources->get($sourceID);
+
+            if ($this->request->is('post')) {
+                $this->Sources->patchEntity($source, $this->request->data);
+                $source->date_from = $this->request->data('date_from');
+                $source->date_to = $this->request->data('date_to');
+
+                if ($this->Sources->save($source)) {
+                    $this->Flash->success('Příspěvek "' . $source->name . '" byl úspěšně změněn!');
+                } else {
+                    $this->Flash->error('Chyba při ukládání změn :(');
+                }
+            }
+        }
+        return $this->redirect($this->redirect(['action' => 'index']));
     }
 
-    function delete(){
+    function delete($sourceID = null){
+        if ($sourceID != null) {
+            $source = $this->Sources->get($sourceID);
+
+            if ($this->Sources->delete($source)) {
+                $this->Flash->success('Příspěvek "' . $source->name . '" byl úspěšně smazán!');
+            } else {
+                $this->Flash->error('Chyba při mazání :(');
+            }
+        }
         return $this->redirect($this->referer());
     }
-
-
 }
