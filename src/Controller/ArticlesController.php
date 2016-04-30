0e188c99e4a873e4692b37bf05487c9a93b0d750
hard456 <janpalcut@seznam.cz>
2016-04-30 16:01:39 +0200

    upload odstranění diakritiky

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -148,6 +148,17 @@ class ArticlesController extends AppController
      */
     function file_upload($file, $validation, $path)
     {
+        $transformation_array = Array(
+            'ä'=>'a','Ä'=>'A','á'=>'a','Á'=>'A','à'=>'a','À'=>'A','ã'=>'a','Ã'=>'A','â'=>'a','Â'=>'A',
+            'č'=>'c','Č'=>'C','ć'=>'c','Ć'=>'C','ď'=>'d','Ď'=>'D','ě'=>'e','Ě'=>'E','é'=>'e','É'=>'E',
+            'ë'=>'e','Ë'=>'E','è'=>'e','È'=>'E','ê'=>'e', 'Ê'=>'E','í'=>'i','Í'=>'I','ï'=>'i','Ï'=>'I',
+            'ì'=>'i','Ì'=>'I','î'=>'i','Î'=>'I','ľ'=>'l','Ľ'=>'L','ĺ'=>'l','Ĺ'=>'L','ń'=>'n','Ń'=>'N',
+            'ň'=>'n','Ň'=>'N','ñ'=>'n','Ñ'=>'N','ó'=>'o','Ó'=>'O','ö'=>'o','Ö'=>'O','ô'=>'o','Ô'=>'O',
+            'ò'=>'o','Ò'=>'O','õ'=>'o','Õ'=>'O','ő'=>'o','Ő'=>'O','ř'=>'r','Ř'=>'R','ŕ'=>'r','Ŕ'=>'R',
+            'š'=>'s','Š'=>'S','ś'=>'s','Ś'=>'S','ť'=>'t','Ť'=>'T','ú'=>'u','Ú'=>'U','ů'=>'u','Ů'=>'U',
+            'ü'=>'u','Ü'=>'U','ù'=>'u','Ù'=>'U','ũ'=>'u','Ũ'=>'U','û'=>'u','Û'=>'U','ý'=>'y','Ý'=>'Y',
+            'ž'=>'z','Ž'=>'Z','ź'=>'z','Ź'=>'Z'
+        );
         foreach ($file as $tmp) {
             if ($tmp['name'] != null) {
                 if (in_array((strtolower(pathinfo($tmp["name"], PATHINFO_EXTENSION))), $validation)) {
@@ -155,38 +166,42 @@ class ArticlesController extends AppController
                         if (!mkdir($path, 0777, true)) {
                             die('Failed to create folders...');
                         } else {
-                            move_uploaded_file($tmp['tmp_name'], $path . '/' . $tmp['name']);
-                        }
-                    } else {
-                        move_uploaded_file($tmp['tmp_name'], $path . '/' . $tmp['name']);
-                    }
+    $tmp['name'] = strtr($tmp['name'], $transformation_array);
 
-                } else {
-                    $this->Flash->error(__('Neplatný typ pro soubor:' . $tmp['name']));
-                }
-            }
-        }
+    move_uploaded_file($tmp['tmp_name'], $path . '/' . $tmp['name']);
+}
+} else {
+    $tmp['name'] = strtr($tmp['name'], $transformation_array);
+    move_uploaded_file($tmp['tmp_name'], $path . '/' . $tmp['name']);
+
+}
+
+} else {
+    $this->Flash->error(__('Neplatný typ pro soubor:' . $tmp['name']));
+}
+}
+}
+}
+
+/**
+ * File size of sigle entities (images, videos, documents, audio)
+ * @param $file file entity
+ * @return int size of files
+ */
+function file_size($file)
+{
+    $input_size = 0;
+    foreach ($file as $f) {
+        $input_size += $f['size'];
     }
+    return $input_size;
+}
 
-    /**
-     * File size of sigle entities (images, videos, documents, audio)
-     * @param $file file entity
-     * @return int size of files
-     */
-    function file_size($file)
-    {
-        $input_size = 0;
-        foreach ($file as $f) {
-            $input_size += $f['size'];
-        }
-        return $input_size;
-    }
-
-    /**
-     * Shows detail of single article
-     * @param null $prispevek_id article id
-     */
-    function detail($prispevek_id = null)
+/**
+ * Shows detail of single article
+ * @param null $prispevek_id article id
+ */
+function detail($prispevek_id = null)
     {
         if ($prispevek_id!=null) {
             $this->loadModel('Sources');
