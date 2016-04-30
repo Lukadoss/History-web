1ee9b27701e560475b6431833d1b533dc5cc9c91
Lukado <lukado222@seznam.cz>
2016-04-30 14:02:29 +0200

    user settings fixed

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -129,18 +129,20 @@ class UsersController extends AppController
     {
         if ($this->request->is('post')){
             $user = $this->Users->get($this->Auth->user('user_id'));
-            $user = $this->Users->patchEntity($user, $this->request->data, ['validate'=>'settings']);
+            $bool = false;
 
+            if (!$this->request->data(['password'])){
+                $bool = true;
+            }elseif(!DefaultPasswordHasher::check($this->request->data('current_password'), $user['password'])) {
+                $user->errors('Check password', ['Chybné staré heslo']);
+            }
+
+            $user = $this->Users->patchEntity($user, $this->request->data, ['validate'=>'settings']);
             if ($this->request->data(['password'])){
                 $user = $this->Users->patchEntity($user, $this->request->data, ['validate'=>'pass']);
             }
 
-            debug($this->request->data);
-            if (!$this->request->data(['password'])){
-                unset($user->password);
-            }elseif(!DefaultPasswordHasher::check($this->request->data('current_password'), $user['password'])) {
-                $user->errors('Check password', ['Chybné staré heslo']);
-            }
+            if ($bool) unset($user->password);
 
             if(!$user->errors()){
                 if(!$this->request->data(['forename']) && !$this->request->data(['surname']) && !$this->request->data(['password'])){
